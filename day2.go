1a525b2e132cc53335771f7ad8689d789401c908
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-02 09:18:12 +0100

    done day2

--- day2.go
+++ day2.go
@@ -59,6 +59,53 @@ func part1(input []string) int {
 	}
 	return c
 }
+
+func part2(input []string) int {
+
+	c := 0
+
+	for i := 0; i < len(input); i++ {
+		in := input[i]
+		if len(in) < 2 {
+			continue
+		}
+		a := input[i][0]
+		b := input[i][2]
+		switch b {
+		case 'X': // Lose
+			if a == 'A' { // Lose from rock: scisor
+				fmt.Println("Losing from rock:scisor")
+				c = c + 3
+			} else if a == 'B' { // Lose from paper: rock
+				fmt.Println("Losing from paper:scisor")
+				c = c + 1
+			} else if a == 'C' { // Lose from scisor: paper
+				c = c + 2
+			}
+		case 'Y': // Draw
+			c = c + 3
+			if a == 'A' { // Draw from rock: rock
+				c = c + 1
+			} else if a == 'B' { // Draw from paper: paper
+				c = c + 2
+			} else if a == 'C' { // Draw from scisor: scisor
+				c = c + 3
+			}
+		case 'Z': //Win
+			c = c + 6
+			if a == 'A' { // Win from rock: paper
+				c = c + 2
+			} else if a == 'B' { // Win from paper: scisor
+				c = c + 3
+			} else if a == 'C' { // Win from scisor: rock
+				c = c + 1
+			}
+		}
+
+	}
+	return c
+}
+
 func day2() {
 
 	godotenv.Read(".env")
@@ -67,6 +114,7 @@ func day2() {
 
 	input := strings.Split(GetInput(2), "\n") // Break the string into an array
 
-	fmt.Println("Solution is:", part1(input))
+	fmt.Println("Part 1 solution is:", part1(input))
+	fmt.Println("Part 2 solution is:", part2(input))
 
 }
