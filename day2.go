2209eb7c3785ab50e6fa138f51b1b96b57304b1b
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-02 10:15:16 +0100

    Cleanup

--- day2.go
+++ day2.go
@@ -74,10 +74,8 @@ func part2(input []string) int {
 		switch b {
 		case 'X': // Lose
 			if a == 'A' { // Lose from rock: scisor
-				fmt.Println("Losing from rock:scisor")
 				c = c + 3
 			} else if a == 'B' { // Lose from paper: rock
-				fmt.Println("Losing from paper:scisor")
 				c = c + 1
 			} else if a == 'C' { // Lose from scisor: paper
 				c = c + 2
