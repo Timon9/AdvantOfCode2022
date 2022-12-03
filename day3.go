72fe4052be95fc6d8dda5981d758cac4e5c5f8e8
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-03 08:59:07 +0100

    Cleanup

--- day3.go
+++ day3.go
@@ -24,11 +24,10 @@ func findDuplicates(a string, b string) int {
 	}
 	for ii := 0; ii < len(b); ii++ {
 		if _, ok := m[b[ii]]; ok {
-
+			// Duplicate is found
 			a := findPriority(rune(b[ii]))
-			// fmt.Println("Found", b[ii:ii+1], "Add", a)
 			r = r + a
-			delete(m, b[ii])
+			delete(m, b[ii]) // Remove from the map to prevent double finding
 		}
 
 	}
@@ -46,7 +45,48 @@ func day3Part1(input []string) int {
 }
 
 func day3Part2(input []string) int {
-	return 0
+	r := 0
+	g := 0
+	m := make(map[byte]int)
+	t := make(map[byte]bool)
+
+	for i := 0; i < len(input); i++ {
+		g++
+		bag := input[i]
+		count := len(bag)
+		for ii := 0; ii < count; ii++ {
+			if _, ok := t[bag[ii]]; !ok {
+				t[bag[ii]] = true
+			}
+
+		}
+		for k := range t {
+			if _, ok := m[k]; ok {
+				// Find duplicate 3x
+				if m[k] > 1 {
+					//Found it
+					d := findPriority(rune(k))
+					r = r + d
+				} else {
+					m[k] = m[k] + 1 // Increment
+				}
+
+			} else {
+				m[k] = 1
+			}
+
+			delete(t, k)
+		}
+
+		if g == 3 { // New group, reset map
+			g = 0
+			for k := range m {
+				delete(m, k)
+			}
+		}
+
+	}
+	return r
 }
 
 func day3() {
@@ -58,6 +98,6 @@ func day3() {
 	input := strings.Split(GetInput(3), "\n") // Break the string into an array
 
 	fmt.Println("Part 1 solution is:", day3Part1(input))
-	//	fmt.Println("Part 2 solution is:", day3Part2(input))
+	fmt.Println("Part 2 solution is:", day3Part2(input))
 
 }
