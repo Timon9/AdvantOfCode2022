49ea0fcb7353987e8331e45c2a620825b57024be
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-05 09:30:04 +0100

    wip

--- day5.go
+++ day5.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-func day5part1(input []string) int {
+func day5Part1(input []string) int {
 	r := 0
-	for i := 0; i < len(input); i++ {
+	// Fill the matrix
+	for i := 1; i < len(input) && i < 8; i++ {
+		line := input[8-i]
+		// crate1 := line[0:3]
+		fmt.Println(line)
+		// fmt.Println(crate1)
+
 	}
 
 	return r
@@ -29,9 +36,8 @@ func day5() {
 
 	fmt.Println("Day 5")
 
-	// input := strings.Split(GetInput(5), "\n") // Break the string into an array
-	input := []string{"123"}
-	fmt.Println("Part 1 solution is:", day4Part1(input))
-	// fmt.Println("Part 2 solution is:", day4Part2(input))
+	input := strings.Split(GetInput(5), "\n") // Break the string into an array
+	fmt.Println("Part 1 solution is:", day5Part1(input))
+	// fmt.Println("Part 2 solution is:", day5Part2(input))
 
 }
