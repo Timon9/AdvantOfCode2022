d6730611f0980b26e87e48e221f6101aacb9ed50
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-05 09:18:55 +0100

    d4

--- day4.go
+++ day4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -10,14 +11,54 @@ import (
 func day4Part1(input []string) int {
 	r := 0
 	for i := 0; i < len(input); i++ {
+
+		pair := strings.Split(input[i], ",")
+		if len(pair) > 1 {
+			s1 := strings.Split(pair[0], "-")
+			s2 := strings.Split(pair[1], "-")
+
+			a1, _ := strconv.Atoi(s1[0])
+			a2, _ := strconv.Atoi(s1[1])
+
+			b1, _ := strconv.Atoi(s2[0])
+			b2, _ := strconv.Atoi(s2[1])
+
+			if a1 <= b1 && a2 >= b2 { // A overlaps B
+				fmt.Println("1:", pair)
+				r++
+			} else if b1 <= a1 && b2 >= a2 { // B overlaps A
+				fmt.Println("2:", pair)
+				r++
+			}
+		}
 	}
+
 	return r
 }
 
 func day4Part2(input []string) int {
 	r := 0
-
 	for i := 0; i < len(input); i++ {
+
+		pair := strings.Split(input[i], ",")
+		if len(pair) > 1 {
+
+			s1 := strings.Split(pair[0], "-")
+			s2 := strings.Split(pair[1], "-")
+
+			a1, _ := strconv.Atoi(s1[0])
+			a2, _ := strconv.Atoi(s1[1])
+
+			b1, _ := strconv.Atoi(s2[0])
+			b2, _ := strconv.Atoi(s2[1])
+
+			if (a1 < b1 && a2 < b1) || (b1 < a1 && b2 < a1) {
+				fmt.Println("No overlap", pair)
+			} else {
+				r++
+			}
+
+		}
 	}
 
 	return r
@@ -30,7 +71,6 @@ func day4() {
 	fmt.Println("Day 4")
 
 	input := strings.Split(GetInput(4), "\n") // Break the string into an array
-
 	fmt.Println("Part 1 solution is:", day4Part1(input))
 	fmt.Println("Part 2 solution is:", day4Part2(input))
 
