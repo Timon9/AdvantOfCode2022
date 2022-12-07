561fa68869463288b1f7c32429adb139394a9628
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-07 09:44:35 +0100

    Cleanup

--- day7.go
+++ day7.go
@@ -8,8 +8,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func findSmallestDir(input string) int {
-
+func buildDirTree(input string) map[string]int {
 	lines := strings.Split(input, "\n")
 	cm := make(map[string]int)
 	rm := []string{}
@@ -50,59 +49,29 @@ func findSmallestDir(input string) int {
 		}
 
 	}
+	return cm
+}
+func findSmallestDir(input string) int {
 
-	r := 0
+	cm := buildDirTree(input)
+	r := 30000000
 
 	totalSize := cm["lv_1_/"]
 	unused := 70000000 - totalSize
+	needed := 30000000 - unused
 
-	fmt.Println("Total size", totalSize, "unused", unused)
+	for k := range cm {
+		if cm[k] >= needed && cm[k] < r {
+			r = cm[k]
+		}
+	}
 
 	return r
 }
 
 func findTotalSizeOfLargeDirectories(input string) int {
 
-	lines := strings.Split(input, "\n")
-	cm := make(map[string]int)
-	rm := []string{}
-
-	dl := false
-
-	ttmp := 0
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		ll := len(line)
-
-		if ll > 1 && line[0:1] == "$" {
-			dl = false
-		}
-
-		if ll > 5 && line[0:5] == "$ cd " {
-			ttmp++
-			switch line[5:ll] {
-			case "..":
-				rm = rm[0 : len(rm)-1]
-				continue
-			default:
-				nextDir := fmt.Sprintf("lv_%d_%v", ttmp, line[5:ll])
-				rm = append(rm, nextDir)
-			}
-		}
-
-		if dl == true && len(line) > 3 && line[0:3] != "dir" {
-			tmp := strings.Split(line, " ")
-			size, _ := strconv.Atoi(tmp[0])
-			for ii := 0; ii < len(rm); ii++ {
-				cm[rm[ii]] = cm[rm[ii]] + size
-			}
-		}
-
-		if ll > 3 && line[0:4] == "$ ls" {
-			dl = true
-		}
-
-	}
+	cm := buildDirTree(input)
 
 	r := 0
 
@@ -116,7 +85,11 @@ func findTotalSizeOfLargeDirectories(input string) int {
 }
 
 func day7Part1(input string) {
-	fmt.Println("Result", findTotalSizeOfLargeDirectories(input))
+	fmt.Println("Result part 1", findTotalSizeOfLargeDirectories(input))
+}
+
+func day7Part2(input string) {
+	fmt.Println("Result part 2", findSmallestDir(input))
 }
 
 func day7() {
@@ -127,5 +100,6 @@ func day7() {
 
 	input := GetInput(7)
 	day7Part1(input)
+	day7Part2(input)
 
 }
