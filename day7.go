44fe249b68eac75bde232dd6b1b4468aaf4d0a6d
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-07 07:49:47 +0100

    Wip

--- day7.go
+++ day7.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
+
+func findTotalSize(input string) int {
+
+	lines := strings.Split(input, "\n")
+	// cm := make(map[string]int) // Hashmap [dir] size
+
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+
+		if len(line) > 5 && line[0:5] == "$ cd " {
+			fmt.Println("Chaging to...")
+		}
+	}
+	return 0
+}
 
 func day7Part1(input string) {
 	fmt.Println("Day7Part1")
