41c21e151e26d37a015de77d2b1e67583537909f
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-07 09:38:34 +0100

    WIP: P2

--- day7.go
+++ day7.go
@@ -8,6 +8,59 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func findSmallestDir(input string) int {
+
+	lines := strings.Split(input, "\n")
+	cm := make(map[string]int)
+	rm := []string{}
+
+	dl := false
+
+	ttmp := 0
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		ll := len(line)
+
+		if ll > 1 && line[0:1] == "$" {
+			dl = false
+		}
+
+		if ll > 5 && line[0:5] == "$ cd " {
+			ttmp++
+			switch line[5:ll] {
+			case "..":
+				rm = rm[0 : len(rm)-1]
+				continue
+			default:
+				nextDir := fmt.Sprintf("lv_%d_%v", ttmp, line[5:ll])
+				rm = append(rm, nextDir)
+			}
+		}
+
+		if dl == true && len(line) > 3 && line[0:3] != "dir" {
+			tmp := strings.Split(line, " ")
+			size, _ := strconv.Atoi(tmp[0])
+			for ii := 0; ii < len(rm); ii++ {
+				cm[rm[ii]] = cm[rm[ii]] + size
+			}
+		}
+
+		if ll > 3 && line[0:4] == "$ ls" {
+			dl = true
+		}
+
+	}
+
+	r := 0
+
+	totalSize := cm["lv_1_/"]
+	unused := 70000000 - totalSize
+
+	fmt.Println("Total size", totalSize, "unused", unused)
+
+	return r
+}
+
 func findTotalSizeOfLargeDirectories(input string) int {
 
 	lines := strings.Split(input, "\n")
@@ -54,7 +107,6 @@ func findTotalSizeOfLargeDirectories(input string) int {
 	r := 0
 
 	for k := range cm {
-		fmt.Println(k, cm[k])
 		if cm[k] < 100000 {
 			r = r + cm[k]
 		}
--- day7_test.go
+++ day7_test.go
@@ -36,3 +36,36 @@ $ ls
 		t.Fatalf("Expected 95437, but got %d", totalSize)
 	}
 }
+
+func TestCanFindSmallestDirectory(t *testing.T) {
+
+	testData := `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`
+
+	totalSize := findSmallestDir(testData)
+
+	if totalSize != 24933642 {
+		t.Fatalf("Test part2: Expected 24933642, but got %d", totalSize)
+	}
+}
