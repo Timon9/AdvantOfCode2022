905777a04a0a73991ba00c54908b85c4ed4e3fe7
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-08 09:38:06 +0100

    wip

--- day8.go
+++ day8.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 func findVisibleTrees(input string) int {
-	return 0
+	r := 0
+	lines := strings.Split(input, "\n")
+	c := len(lines)
+	a := 0
+	for i := 0; i < len(lines[0]); i++ {
+		a++
+	}
+
+	out := c + (a - 1) + (a - 1) + (c - 2)
+
+	for i := 1; i < len(lines)-1; i++ {
+		for ii := 1; ii < len(lines[i])-1; ii++ {
+			fmt.Printf("%v,", string(lines[i][ii]))
+		}
+
+	}
+	r = out
+	return r
 }
 
 func day8Part1(input string) {
